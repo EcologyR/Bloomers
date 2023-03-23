556caf017ef706fe20f6bd4277dfb7be5a6602bd
iremendoza <irene.mendoza@ebd.csic.es>
2023-03-23 10:24:51 +0100

    multiply_by function
    created and documented. Close #2

---
+++ R/multiply_by.R
@@ -0,0 +1,16 @@
+#' Multiply by a give value (default is ten)
+#'
+#' @param x Numeric
+#' @param y Numeric (10 by default)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' multipy_by(2)
+#' multiply_by(2,100)
+
+multiyply_by <- function(x = Null, y = 10){
+
+  x * y
+}
