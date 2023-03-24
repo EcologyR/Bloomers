92bef0f3d4162198a8f492139c6fb99a14c060cb
onadeulofeu <ona.deulofeu@gmail.com>
2023-03-24 11:59:40 +0100

    create example data

--- data-raw/exampledata_prep.R
+++ data-raw/exampledata_prep.R
@@ -1,13 +1,12 @@
-exampledata_prepare = function (filesamples = "./data-raw/HF_WS_abun_nut_chl_CTD.csv",
+exampledata_prepare = function (abund_data = "./data-raw/example_abund_data.txt",
                              asv_tab_l = "./data-raw/example_asv_tab_long.txt",
-                             metadata = "./data-raw/HFW_HFS_metadaata.csv") {
+                             metadata = "./data-raw/example_metadata.csv") {
 
 abund_data <- read.csv2(file = filesamples)
-asv_tab_l <- read.delim(file = filesamplecodes)
+asv_tab_l <- read.delim(file = asv_tab_l)
 metadata <- read.csv2(file = metadata)
-
 
 }
 
-usethis::use_data(exampledata, overwrite = TRUE)
+usethis::use_data(exampledata_prepare, overwrite = TRUE)
 #con esto se crea un dataset de ejemplo que se carga con el paquete
