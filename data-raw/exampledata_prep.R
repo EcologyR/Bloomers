43110421693026b178541a56762296a44dcc69b0
onadeulofeu <ona.deulofeu@gmail.com>
2023-03-24 11:51:21 +0100

    get example data

--- data-raw/exampledata_prep.R
+++ data-raw/exampledata_prep.R
@@ -1,9 +1,9 @@
 exampledata_prepare = function (filesamples = "./data-raw/HF_WS_abun_nut_chl_CTD.csv",
-                             filesamplecodes = "./data-raw/sample_codes_HF_MIAU.txt",
+                             asv_tab_l = "./data-raw/example_asv_tab_long.txt",
                              metadata = "./data-raw/HFW_HFS_metadaata.csv") {
 
-samples <- read.csv2(file = filesamples)
-codes <- read.delim(file = filesamplecodes)
+abund_data <- read.csv2(file = filesamples)
+asv_tab_l <- read.delim(file = filesamplecodes)
 metadata <- read.csv2(file = metadata)
 
 
