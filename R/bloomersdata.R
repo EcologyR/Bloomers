dfde4e888bdffb3f8fdcae82fa33d707d301ca3e
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 20:47:33 +0200

    documentation for bloomersdata created

---
+++ R/bloomersdata.R
@@ -0,0 +1,19 @@
+#' an example dataset of bloomers with 9 ASVs or taxa
+#'
+#' @name bloomersdata
+#' @docType data
+#' @author O. Deulofeu-Capo, I. Bartomeus, I. Mendoza \email{ona@@roxygen.org}
+#' @references \url{relative_abundances.com}
+#' @keywords marine microbes, bloom
+#' \itemize{
+#'   \item taxon: ASV
+#'   \item date_hour: a combination in one string of the date and time of sampling
+#'   \item pseudoabundance: pseudoabundance value for that sample
+#'
+#'   }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name bloomersdata
+#' @usage data(bloomersdata)
+#' @format A data frame with 3 variables
