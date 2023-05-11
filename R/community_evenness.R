776abe83564c9dd07f679bdabcc477952fb1b8ca
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 11:01:07 +0200

    library tidyr added to function community_evenness.R

--- R/community_evenness.R
+++ R/community_evenness.R
@@ -52,6 +52,7 @@ community_evenness <- function(abundances, index = "Shannon") {
 ## Example with bloomers data
 
 load("./data/bloomersdata.rda")
+library(tidyr)
 
 bloomers_abund <- bloomersdata %>% pivot_wider(names_from = date_hour, values_from = pseudoabundance)
 
