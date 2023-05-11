9b6df3fe344421eec03f9ed96499fd32fd781625
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 10:48:26 +0200

    minor edit of format

--- R/community_evenness.R
+++ R/community_evenness.R
@@ -35,7 +35,9 @@ community_evenness <- function(abundances, index = "Shannon") {
     pi <- abundances / N
     D <- sum(pi^2)
     E <- 1/D
-  } else if (index == "Pielou"){
+  }
+
+  else if (index == "Pielou"){
 
     E <- -sum((abundances/sum(abundances))*log(abundances/sum(abundances)))
   }
