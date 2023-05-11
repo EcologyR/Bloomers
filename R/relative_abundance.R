d45c2bf8e1179ca55c7540fa428ba728a714a0a5
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 20:45:28 +0200

    documentation for relative_abundance dataset created

---
+++ R/relative_abundance.R
@@ -0,0 +1,19 @@
+#' relative_abundances extracted from reads of ASVs or taxa
+#'
+#' @name relative_abundances
+#' @docType data
+#' @author O. Deulofeu-Capo, I. Bartomeus, I. Mendoza \email{ona@@roxygen.org}
+#' @references \url{relative_abundances.com}
+#' @keywords marine microbes, bloom
+#' \itemize{
+#'   \item taxa: ASV
+#'   \item abund: number of reads of each taxa/ASV
+#'   \item sample_id: unique identification event of sampling
+#'
+#'   }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name relatabun
+#' @usage data(relatabun)
+#' @format A data frame with 3 variables
