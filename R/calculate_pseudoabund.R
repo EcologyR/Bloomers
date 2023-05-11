b3f92e5c0e873d406d162a832a47002f92c957f2
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 13:24:10 +0200

    example for pseudoabundance included

--- R/calculate_pseudoabund.R
+++ R/calculate_pseudoabund.R
@@ -3,7 +3,7 @@
 #'
 #' @param data a tidy tibble long with relative abundance and total abundance per sample as columns
 #' @param abund_data a tibble with total abundance data
-#' @param rel_abund abundance column with relative abundances (0-1) for each asv from the data tibble
+#' @param rel_abund abundance column with relative abundances (0-1) for each taxa from the data tibble
 #' @param total_abund column with total abundance data from abund_data tibble
 #'
 #' @return
@@ -13,6 +13,14 @@
 #' @import dplyr
 #'
 #' @examples
+#' taxa <- c("taxa1", "taxa2", "taxa3", "taxa4")
+#' rel.abundances<-  c(runif(16, 0, 2000), runif(16, 0, 2000), runif(16, 0, 2000), runif(16, 0, 2000))
+#' data <- data.frame(taxa = rep(taxa, 16), rel.abundances, event = sort(rep(1:16,4)))
+#' abund_data <- data.frame(event = 1:16, tot.abund = runif(16, 2000, 5000))
+#' calculate_pseudoabund(data, abund_data, rel_abund = rel.abundances, total_abund = tot.abund)
+
+#'@author O. Deulofeu
+
 calculate_pseudoabund <- function(data, abund_data, by_ = NULL, #sample_id1 = "col1", sample_id2 = "col2",
                                   rel_abund, total_abund){
   if("package:plyr" %in% search()) detach("package:plyr", unload=TRUE)
