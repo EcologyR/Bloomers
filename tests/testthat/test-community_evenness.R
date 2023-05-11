2b649016460570843a186246854a8183b058ac12
iremendoza <irene.mendoza@ebd.csic.es>
2023-05-11 11:22:38 +0200

    tests corrected

--- tests/testthat/test-community_evenness.R
+++ tests/testthat/test-community_evenness.R
@@ -1,26 +1,26 @@
 library(testthat)
 
 # Define a test file for the function
-test_file("test-community_evenness.R")
+test_file("tests/testthat/test-community_evenness.R")
 
 # Write test cases using expect_equal() function
 test_that("community_evenness calculates Shannon index correctly", {
   abundances <- c(5, 2, 3, 1, 4)
-  expected_E <- 0.8453488
+  expected_E <- 0.92563392
   calculated_E <- community_evenness(abundances, index = "Shannon")
   expect_equal(calculated_E, expected_E)
 })
 
 test_that("community_evenness calculates Simpson index correctly", {
-  abundances <- c(5, 2, 3, 1, 4)
-  expected_E <- 0.8613861
+  abundances <- c(40,27,5,6,9)
+  expected_E <- 3.0631323
   calculated_E <- community_evenness(abundances, index = "Simpson")
   expect_equal(calculated_E, expected_E)
 })
 
 test_that("community_evenness calculates Pielou index correctly", {
-  abundances <- c(5, 2, 3, 1, 4)
-  expected_E <- 0.7446436
+  abundances <- c(50, 2, 35, 10, 4)
+  expected_E <- 1.149817
   calculated_E <- community_evenness(abundances, index = "Pielou")
   expect_equal(calculated_E, expected_E)
 })
