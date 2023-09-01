273100b1369e619c89fb516bc5ba98971b7349c6
onadeulofeu <ona.deulofeu@gmail.com>
2023-09-01 11:49:10 +0200

    update function find ASVs with anomalies adding another possible condition to fulfill

--- R/find_asv_with_anomalies.R
+++ R/find_asv_with_anomalies.R
@@ -18,18 +18,22 @@
 #'   logic2 = TRUE,
 #'  asv_col = asv_num)
 #'
-find_asv_with_anomalies <- function(anomalies_result, anomaly_in1, anomaly_in2, logic1 = TRUE, logic2 = TRUE, asv_col = asv_num){
-  # if(is.list(z) == FALSE){
+find_asv_with_anomalies <- function(anomalies_result, anomaly_in1, anomaly_in2,
+                                    anomaly_in3 = NULL, logic1 = TRUE,
+                                    logic2 = TRUE,logic3 = NULL,
+                                    asv_col = asv_num) {
+  # if(is.list(anomalies_result) == FALSE){
   #   stop("Function stopped: anomalies_result needs to be a list form the get_anomalies function")
   # }
   # if(is.logical({{anomaly_in1}}) == FALSE){
   #   stop("Function stopped: anomaly_in1 needs to be logical (TRUE/FALSE)")
   # }
-  #
+
   asv_potential_bloomers <-
     anomalies_result |>
-    dplyr::filter({{anomaly_in1}} %in% logic1 &
-                    {{anomaly_in2}} %in% logic2) |>
+    dplyr::filter(if (!is.null(logic1)) {{anomaly_in1}} %in% logic1 else TRUE) |>
+    dplyr::filter(if (!is.null(logic2)) {{anomaly_in2}} %in% logic2 else TRUE) |>
+    dplyr::filter(if (!is.null(logic3)) {{anomaly_in3}} %in% logic3 else TRUE) |>
     dplyr::select({{asv_col}}) |>
     as_vector()
 
